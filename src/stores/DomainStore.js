2fe100aa64a0689906e57a19c36aa66079fe9aeb
Daniel Tam <daniel.tam@accenture.com>
2018-02-20 18:35:20 +1100

    Add domainStore scaffold

--- src/stores/DomainStore.js
+++ src/stores/DomainStore.js
@@ -1,7 +1,27 @@
 import { action, computed, observable } from 'mobx';
 
 class DomainStore {
-  @observable Foo='bar';
+  @observable foo = 'bar';
+  @observable polls = [
+    {
+      id: 1,
+      name: "PollName",
+      description: "This is a great poll",
+      // votes: 69,
+      created: Date.now(),
+      expires: Date.now() + 4,
+      results: [
+        {
+          name: "John",
+          votes: 4
+        },
+        {
+          name: "Bar",
+          votes: 5
+        }
+      ]
+    }
+  ]
 }
 
 export default DomainStore;
