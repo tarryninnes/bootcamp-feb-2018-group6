0b20cdd0238b6ac3c608c7821ff764a51218c94c
Daniel Tam <daniel.tam@accenture.com>
2018-02-20 12:59:11 +1100

    Add framework to front end

---
+++ src/components/Bar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { observable } from 'mobx';
+
+
+
+const Bar = (props) => {
+
+  return(
+    <div>
+      {props.toString()}
+      {}
+    </div>
+  );
+};
+
+export default Bar;
